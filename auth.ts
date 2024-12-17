254fdaac362722b09c5db198c84ff48f1615033c
robuGdada <paudelronish@gmail.com>
2024-12-17 20:44:22 +0545

    next auth,shacn and global next js config

---
+++ auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export const { auth, signIn, signOut } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+});
