3b15ecb86c43816fb80ae80307070375a4325d7f
robuGdada <paudelronish@gmail.com>
2024-12-17 20:59:35 +0545

    flower svg

---
+++ components/Blooming-Flower.tsx
@@ -0,0 +1,18 @@
+export function BloomingFlower() {
+  return (
+    <svg
+      width="120"
+      height="120"
+      viewBox="0 0 120 120"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="60" cy="60" r="50" fill="#FDE68A" />
+      <path
+        d="M60 10C60 10 70 30 90 30C110 30 110 10 110 10C110 10 110 30 110 50C110 70 90 80 90 80C90 80 110 90 110 110C110 110 90 110 70 90C50 110 30 110 30 110C30 110 50 90 50 70C30 70 10 50 10 50C10 50 30 50 50 30C30 10 10 10 10 10C10 10 30 30 50 30C70 30 80 10 80 10"
+        fill="#FCD34D"
+      />
+      <circle cx="60" cy="60" r="20" fill="#FBBF24" />
+    </svg>
+  );
+}
